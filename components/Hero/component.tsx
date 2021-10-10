83396ccccd1ac565d78e168b48480f3ac9379e09
ARAswap <80604400+P0DWOG@users.noreply.github.com>
2021-10-10 16:15:39 -0400

    Update component.tsx

--- components/Hero/component.tsx
+++ components/Hero/component.tsx
@@ -20,15 +20,15 @@ export const Hero: FC = () => {
   return (
     <div>
       <h1 className="text-4xl font-bold md:mt-40 md:mb-2 md:text-5xl text-white-900">
-        Save the world from COVID-19
+        Plant trees with Energuy !
       </h1>
       <div>
         <p className="mb-1 text-3xl text-white-700">
-          Deposit crypto and save people's lifes
+          Deposit your DAI and make the planet greener:
         </p>
         <p className="mb-2 text-3xl text-white-500">
-          <span className="text-blue-600">{interest.substr(0, 18)}</span> DAI
-          generated already
+          <span className="text-blue-600">{interest.substr(0, 18)}</span> Trees
+          planted already !
         </p>
       </div>
     </div>
