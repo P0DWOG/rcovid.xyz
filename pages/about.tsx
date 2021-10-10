dc159e57f7116aef79ba84122771a5ba5f8013de
ARAswap <80604400+P0DWOG@users.noreply.github.com>
2021-10-10 16:50:22 -0400

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,36 +7,34 @@ const AboutPage = () => {
         What's going on?
       </p>
       <p className="mt-4 text-lg text-white-700">
-        This website is an example of how people can donate crypto assets to
-        charities without actually spending them.
+        Welcome to this example for Cam !
       </p>
       <p className="mt-4 text-lg text-white-700">
-        We use the rDAI protocol, allowing people to earn interest on their DAI
+        We allow people to earn interest on their DAI
         and redirect it to charity organisations.
       </p>
       <p className="mt-4 text-lg text-white-700">
-        rDAI uses Compound to allow users to earn interest on DAI, but instead
+        Aave allows users to earn interest on their DAI, but instead
         of redeeming the interest and principal back into the user’s wallet as
-        one might do with a traditional bank savings account, it separates the
-        principal from the interest. The principal is always under the original
-        user’s control, but the interest is directed to flow wherever the rDAI
-        instructions specify.
+        with a traditional bank savings account; we separate the
+        principal from the interest-- The principal is always under the original
+        user’s control, but the interest is redirected to grow the planet !
       </p>
       <p className="mt-8 mb-4 text-2xl font-bold text-white-900">
-        I want to create something similar!
+        I want to support something, but I hate trees !
       </p>
       <p className="mt-4 text-lg text-white-700">
-        The code is fully{" "}
+        No problem ! The code is fully{" "}
         <a
-          href="https://github.com/ironsoul0/rcovid.xyz"
+          href="https://github.com/MuneFinance/MuneFinance"
           target="_blank"
           rel="noreferrer"
           className="text-blue-500 underline"
         >
-          open-source
+          open-sourced
         </a>{" "}
-        and was intended to be forked by other people. So please go through the
-        README in the repo to set up your website with possibly custom design.
+        and was intended to be forked. So please go through the
+        README in the repo and support your own cause !
       </p>
     </div>
   );
